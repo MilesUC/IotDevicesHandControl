8e9a828658b81193dd98179d9837fa8a327ef875
DIEGO MILLA <diego.milla@uc.cl>
2023-07-30 00:49:23 -0400

    Functions added

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""This module has components that are used for testing tuya's device control and Pulsar massage queue."""
 import logging
 from modules import (
     TuyaOpenAPI,
@@ -6,10 +5,42 @@ from modules import (
 )
 
 from config import settings
-import mouse
 import cv2
 import mediapipe as mp
 import math
+
+def DistanceBetweenIndexAndThumb(hand_landmarks, mp_hands):
+    hd = hand_landmarks.landmark
+    mp = mp_hands.HandLandmark
+    return math.dist(
+        [hd[mp.INDEX_FINGER_TIP].x, hd[mp.INDEX_FINGER_TIP].y], 
+        [hd[mp.THUMB_TIP].x, hd[mp.THUMB_TIP].y]
+        )
+
+#Fixme: not working
+def GetAvailableDevices(openapi, devices):
+    # List of available devices
+    available_devices = []
+
+    # Get devices status
+    for device in devices:
+        response = openapi.post("/v1.0/iot-03/devices/{}/commands".format(devices[0]), {'commands': []})
+        if response['success'] == True:
+            available_devices.append(device)
+
+    return available_devices
+
+def ChangeDeviceStatus(openapi, device):
+    # Get device current status
+    response = openapi.get("/v1.0/iot-03/devices/{}/status".format(device))
+
+    # New status
+    value = not response['result'][0]['value']
+
+    # Post new status
+    commands = {'commands': [{'code': 'switch_led', 'value': value}]}
+    openapi.post('/v1.0/iot-03/devices/{}/commands'.format(device), commands)
+
 
 def SmartAPI(api_endpoint, access_id, access_key, mq_endpoint, devices):
 
@@ -28,9 +59,10 @@ def SmartAPI(api_endpoint, access_id, access_key, mq_endpoint, devices):
     cap = cv2.VideoCapture(0)
 
     with mp_hands.Hands(
-            model_complexity=0,
+            model_complexity=1,
             min_detection_confidence=0.5,
-            min_tracking_confidence=0.5) as hands:
+            min_tracking_confidence=0.5,
+            max_num_hands=1) as hands:
         while cap.isOpened():
             success, image = cap.read()
             if not success:
@@ -42,38 +74,21 @@ def SmartAPI(api_endpoint, access_id, access_key, mq_endpoint, devices):
 
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            #mouse.move(100, 100, absolute=False, duration=0.2)
+
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
-                    #print(results.multi_handedness)
-                    d1 = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)**2)
-                    d2 = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)**2)
-                    d3 = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)**2)
-                    d4 = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)**2)
-                    d5 = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)**2)
-                    dt = d3 + d4 + d5
-                    dn = math.sqrt((hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y-hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)**2+(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x-hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x)**2)
-                    #print(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)
-                    if dt < 0.8:
-                        if dn < 0.03:
 
-                            # Devices handling
-                            for device in devices:
+                    d = DistanceBetweenIndexAndThumb(hand_landmarks, mp_hands)
+                    if d < 0.03:
+                        for device in devices:
+                            ChangeDeviceStatus(openapi, device)
 
-                                response = openapi.get("/v1.0/iot-03/devices/{}/status".format(device))
-
-                                value = not response['result'][0]['value']
-
-                                # Send commands
-                                commands = {'commands': [{'code': 'switch_led', 'value': value}]}
-                                openapi.post('/v1.0/iot-03/devices/{}/commands'.format(device), commands)
-
-                        mp_drawing.draw_landmarks(
-                            image,
-                            hand_landmarks,
-                            mp_hands.HAND_CONNECTIONS,
-                            mp_drawing_styles.get_default_hand_landmarks_style(),
-                            mp_drawing_styles.get_default_hand_connections_style())
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
             cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
             if cv2.waitKey(5) & 0xFF == 27:
                 break
